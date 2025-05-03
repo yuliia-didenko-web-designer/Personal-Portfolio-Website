613145f40aafdaed5f385a907f7e681c1b687dc6
Yuliia Didenko <yuliia.didenko@powercoders.org>
2025-05-03 12:10:40 +0200

    my almost ready web site

---
+++ cursor.js
@@ -0,0 +1,29 @@
+    
+    let cursorDot = document.querySelector(".cursor-dot");
+    let cursorOutline = document.querySelector(".cursor-outline");
+
+    window.addEventListener("mousemove", function (e) {
+      let posX = e.clientX;
+      let posY = e.clientY;
+
+      cursorDot.style.left = `${posX}px`;
+      cursorDot.style.top = `${posY}px`;
+
+      cursorOutline.style.left = `${posX}px`;
+      cursorOutline.style.top = `${posY}px`;
+
+      cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+      }, { duration: 500, fill: "forwards" });
+    });
+
+
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 900) {
+        document.querySelector(".hero-screen").classList.add("disassemble");
+      } else {
+        document.querySelector(".hero-screen").classList.remove("disassemble");
+      }
+    });
