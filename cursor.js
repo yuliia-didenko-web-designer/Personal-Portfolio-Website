db2111c78a56507ed1d3867e202f0af9e02bac5d
Yuliia Didenko <yuliia.didenko@powercoders.org>
2025-05-05 13:31:37 +0200

    website mvp is ready

--- cursor.js
+++ cursor.js
@@ -1,7 +1,10 @@
 let cursorDot = document.querySelector(".cursor-dot");
 let cursorOutline = document.querySelector(".cursor-outline");
 
-window.addEventListener("mousemove", function (e) {
+function isTouchDevice() {
+  return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+}
+function moveCursor(e) {
   let posX = e.clientX;
   let posY = e.clientY;
 
@@ -11,11 +14,32 @@ window.addEventListener("mousemove", function (e) {
   cursorOutline.style.left = `${posX}px`;
   cursorOutline.style.top = `${posY}px`;
 
-  cursorOutline.animate({
-    left: `${posX}px`,
-    top: `${posY}px`
-  }, { duration: 500, fill: "forwards" });
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 500, fill: "forwards" }
+  );
+}
+
+if (!isTouchDevice()) {
+  window.addEventListener("mousemove", moveCursor);
+} else {
+  cursorDot.style.display = "none";
+  cursorOutline.style.display = "none";
+  document.body.style.cursor = "auto";
+}
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 900) {
+    document.querySelector(".hero-screen").classList.add("assemble");
+  } else {
+    document.querySelector(".hero-screen").classList.remove("assemble");
+  }
 });
+
+
 
 
 
